6a235d735e9f21e8a0b02176334b195ff98d4abb
VeTaL2022 <vetal.kots2003@gmail.com>
2022-07-21 22:56:42 +0300

    Initial commit

---
+++ Homework_1/main.js
@@ -0,0 +1,94 @@
+//First task ever using JavaScript!!!
+//1-2 lines of the task!
+    let welcome_text = 'hello';
+    let name_of_site = 'owu';
+    let domain_name = 'com';
+    let text_ua = 'ua';
+    let one = 1;
+    let ten = 10;
+    let minusNine = -999;
+    const one_two_three = 123;
+    const pi = 3.14;
+    const e = 2.7;
+    const sixteen = 16;
+    const bool_true = true;
+    const bool_false = false;
+
+    alert(welcome_text);
+    document.write(welcome_text+ "<br/>");
+    console.log(welcome_text);
+
+    alert(name_of_site);
+    document.write(name_of_site);
+    console.log(name_of_site);
+
+    alert(domain_name);
+    document.write(domain_name+ "<br/>");
+    console.log(domain_name);
+
+    alert(text_ua);
+    document.write(text_ua+ "<br/>");
+    console.log(text_ua);
+
+    alert(one);
+    document.write(""+one+ "<br/>");
+    console.log(one);
+
+    alert(ten);
+    document.write(""+ten+ "<br/>");
+    console.log(ten);
+
+    alert(minusNine);
+    document.write(""+minusNine+ "<br/>");
+    console.log(minusNine);
+
+    alert(one_two_three);
+    document.write(""+one_two_three+ "<br/>");
+    console.log(one_two_three);
+
+    alert(pi);
+    document.write(""+pi+ "<br/>");
+    console.log(pi);
+
+    alert(e);
+    document.write(""+e+ "<br/>");
+    console.log(e);
+
+    alert(sixteen);
+    document.write(""+sixteen+ "<br/>");
+    console.log(sixteen);
+
+    alert(bool_true);
+    document.write(""+bool_true + "<br/>");
+    console.log(bool_true);
+
+    alert(bool_false);
+    document.write(""+bool_false);
+    console.log(bool_false);
+
+
+//4 line of the task!
+    let firstName = 'Kots';
+    let middleName = 'Vitaliy';
+    let lastName = 'Romanovych';
+
+    const person = firstName + ' ' + middleName + ' ' + lastName;
+    console.log(person);//Kots Vitaliy Romanovych
+
+
+//6-7 lines of the task!
+    let a = 100;
+    let b = '100';
+    let c = true;
+    console.log(typeof a);//number
+    console.log(typeof b);//string
+    console.log(typeof c);//boolean
+
+
+//9 line of the task!
+    let name = prompt("What is your name? :");
+    let patronymic = prompt("What is your patronymic? :");
+    let years = prompt("How old are you? :");
+
+    console.log(name + " " + patronymic + " - " + years + " years");
+    // console.log(years + " years");
